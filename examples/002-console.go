6c79c406431709e81c603558de87e365d84954c8
xagero <xagero@gmail.com>
2024-03-20 18:26:09 +0300

    Project files

---
+++ examples/002-console.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/xagero/go-cli"
+	"github.com/xagero/go-cli/command"
+	"os"
+)
+
+func main() {
+
+	name := "console"
+	description := "Simple console application"
+	version := "v0.1"
+
+	console := cli.Construct(name, description, version)
+
+	first := command.Construct("app:first", "First command")
+	first.SetCallback(func() error {
+		fmt.Println("I am first command")
+		return nil
+	})
+	console.AddCommand(first)
+
+	second := command.Construct("app:second", "Second command")
+	second.SetCallback(func() error {
+		fmt.Println("I am second command")
+		return nil
+	})
+	console.AddCommand(second)
+
+	console.SetDefaultCommand(first)
+
+	if err := console.Run(context.Background(), os.Args); err != nil {
+		fmt.Printf("Error encountered: %v\n", err)
+	}
+}
