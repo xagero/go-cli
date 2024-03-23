462c4824426cc554859678a9b726aabfea27fff7
xagero <xagero@gmail.com>
2024-03-23 20:03:36 +0300

    Simple table to improve view

---
+++ examples/006-console-view-table.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/xagero/go-cli"
+	"github.com/xagero/go-cli/command"
+	"github.com/xagero/go-cli/view"
+)
+
+func main() {
+
+	cmd := command.Construct("app:cmd", "Show table view example")
+	cmd.DisableCommonOptions()
+	cmd.SetCallback(func() error {
+
+		tbl := view.Construct("id", "name", "description")
+		tbl.SetHeading("Simple table view")
+		tbl.AddRow(1, "Hello", "Some description at first row")
+		tbl.AddRow(2, "World", "Some description at second row")
+		tbl.Render()
+
+		return nil
+	})
+
+	name := "Console"
+	desc := "Simple console application"
+	version := "v0.1"
+
+	console := cli.Construct(name, desc, version)
+	console.AddCommand(cmd)
+
+	if err := console.Run(context.Background(), os.Args); err != nil {
+		fmt.Printf("Error encountered: %v\n", err)
+	}
+
+}
---
+++ view/table.go
@@ -0,0 +1,163 @@
+package view
+
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+
+	"github.com/xagero/go-helper/helper"
+)
+
+type Table interface {
+	SetHeading(heading string) Table
+	AddRow(values ...interface{}) Table
+	Render()
+}
+
+type table struct {
+
+	// Config
+	padding int
+
+	// Data
+	heading string
+	header  []string
+	rows    [][]string
+
+	// Processed
+	columnWidth []int
+	totalWidth  int
+}
+
+func Construct(headers ...interface{}) Table {
+	// Create new table
+	table := new(table)
+
+	// Configure
+	table.heading = ""
+	table.padding = 4
+	table.header = make([]string, len(headers))
+
+	// Setup header column
+	for i, col := range headers {
+		table.header[i] = fmt.Sprint(col)
+	}
+
+	return table
+}
+
+// SetHeading Set table heading
+func (table *table) SetHeading(heading string) Table {
+	table.heading = heading
+	return table
+}
+
+// AddRow insert new row into Table
+func (table *table) AddRow(values ...interface{}) Table {
+
+	counter := 0 // column
+	for _, val := range values {
+		counter = helper.Max(strings.Count(fmt.Sprint(val), "\n"), counter)
+	}
+
+	for i := 0; i <= counter; i++ {
+		row := make([]string, len(table.header))
+		for j, val := range values {
+			if j >= len(table.header) {
+				break
+			}
+			v := strings.Split(fmt.Sprint(val), "\n")
+			row[j] = offset(v, i)
+		}
+		table.rows = append(table.rows, row)
+	}
+
+	return table
+}
+
+// Render print the Table
+func (table *table) Render() {
+	format := strings.Repeat("%s", len(table.header)) + "\n"
+
+	table.process()
+
+	table.printHeading()
+	table.printTableBetweenRowLine()
+	table.printHeader(format)
+	table.printTableBetweenRowLine()
+
+	for _, row := range table.rows {
+		table.printRow(format, row)
+	}
+
+	table.printTableBetweenRowLine()
+}
+
+func (table *table) printTableBetweenRowLine() {
+	fmt.Println(strings.Repeat("-", table.totalWidth))
+}
+
+func (table *table) printHeading() {
+	if helper.IsNotBlank(table.heading) {
+		fmt.Println("  " + table.heading)
+	}
+}
+
+func (table *table) printHeader(format string) {
+	values := table.applyWidth(table.header, table.columnWidth)
+	fmt.Printf(format, values...)
+}
+
+func (table *table) printRow(format string, row []string) {
+	values := table.applyWidth(row, table.columnWidth)
+	fmt.Printf(format, values...)
+}
+
+func (table *table) applyWidth(row []string, widths []int) []interface{} {
+	out := make([]interface{}, len(row))
+	for i, s := range row {
+		out[i] = s + table.length(s, widths[i]) // @todo text align
+	}
+	return out
+}
+
+func (table *table) length(s string, w int) string {
+	count := w - utf8.RuneCountInString(s)
+	if count <= 0 {
+		return ""
+	}
+	return strings.Repeat(" ", count)
+}
+
+func offset(arr []string, idx int) string {
+	if idx >= len(arr) {
+		return ""
+	}
+	return arr[idx]
+}
+
+func (table *table) process() {
+
+	table.totalWidth = 0
+	table.columnWidth = make([]int, len(table.header))
+
+	for _, row := range table.rows {
+		for i, v := range row {
+			w := utf8.RuneCountInString(v) + table.padding
+			if w > table.columnWidth[i] {
+				table.columnWidth[i] = w
+			}
+		}
+	}
+
+	for i, v := range table.header {
+		w := utf8.RuneCountInString(v) + table.padding
+		if w > table.columnWidth[i] {
+			table.columnWidth[i] = w
+		}
+	}
+
+	for _, w := range table.columnWidth {
+		table.totalWidth += w
+	}
+}
